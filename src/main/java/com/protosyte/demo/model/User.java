44f152d277f283f63a001ec3b8e77a15e83002e9
LssLn <lssln97@gmail.com>
2022-06-05 23:08:57 +0200

    RedditProtosyte development

--- src/main/java/com/protosyte/demo/model/User.java
+++ src/main/java/com/protosyte/demo/model/User.java
@@ -28,4 +28,5 @@ public class User {
 	private String email;
 	private Instant created;
 	private boolean enabled;
+	
 }
--- src/main/java/com/protosyte/demo/service/AuthService.java
+++ src/main/java/com/protosyte/demo/service/AuthService.java
@@ -1,5 +1,7 @@
 package com.protosyte.demo.service;
 
+import java.time.Instant;
+
 import org.springframework.stereotype.Service;
 
 import com.protosyte.demo.dto.RegisterRequest;
@@ -10,6 +12,9 @@ public class AuthService {
 	public void signup(RegisterRequest registerRequest) {
 		User user = new User();
 		user.setUsername(registerRequest.getUsername());
-		
+		user.setPassword(registerRequest.getPassword());
+		user.setEmail(registerRequest.getEmail());
+		user.setCreated(Instant.now());
+		user.setEnabled(false);
 	}
 }
